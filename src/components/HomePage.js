bc9a811dae17f2ab07207e43421ec14f7d81d381
Justin Lohner <lohner4@gmail.com>
2020-04-24 09:40:21 -0600

    Added homepage and pizza form components

---
+++ src/components/HomePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const HomePage = () => {
+	return (
+		<>
+			<h1>Lambda Eats</h1>
+			<p>Welcome to the Lambda Eats Homepage!</p>
+			<button>Pizza?</button>
+		</>
+	);
+};
+export default HomePage;
